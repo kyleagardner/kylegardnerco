eff636601265145755df12595b5aa96413767aba
kyleagardner <kylegardner@MacBook-Pro.local>
2019-08-15 13:34:05 -0700

    updated 404 styling

--- src/pages/404.js
+++ src/pages/404.js
@@ -12,6 +12,26 @@ const FullWidthWrapper = styled.div`
   justify-content: center;
   align-items: center;
   color: #022c2a;
+  flex-direction: column;
+`
+
+const ButtonHome = styled(Link)`
+  text-decoration: none;
+  max-width: 200px;
+  text-align: center;
+  letter-spacing: 0.2px;
+  background-color: #1ebfb5;
+  padding: 0.7rem 2rem;
+  color: #ffffff;
+  font-weight: 600;
+  font-size: 0.95rem;
+  text-transform: uppercase;
+  transition: 0.2s ease-in-out;
+  margin-top: 2.5rem;
+
+  &:hover {
+    background-color: #149890;
+  }
 `
 
 const NotFoundPage = () => (
@@ -19,7 +39,7 @@ const NotFoundPage = () => (
     <FullWidthWrapper>
       <SEO title="404: Not found" />
       <h2>This page does not exist!</h2>
-      <Link to="/">Go Home</Link>
+      <ButtonHome to="/">Go Home</ButtonHome>
     </FullWidthWrapper>
   </Layout>
 )
